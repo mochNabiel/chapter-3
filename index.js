94d20b2133f5b6464951a1bcafd57e14bc0002e5
Mochammad nabiel <mochnabiel77@gmail.com>
2024-03-15 05:49:02 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,151 @@
+/* 
+    Importing module in backend (server) is using require, it is different to frontend that use import from 
+    In this case, we use fs, to read and write file in the backend (server) side    
+*/
+const express = require("express");
+const students = require("./data/students.json");
+const route = require("./route");
+
+/* Initiate express app */
+const app = express();
+const port = 3000;
+
+/* Enable request body (json) */
+app.use(express.json());
+
+/*  
+    Enable static 
+    It will enable the public directory can be loaded, if not loaded we can not access the index.html in the public directory
+*/
+app.use(express.static("public"));
+
+app.use("/", route);
+
+app.get("/students/:id", (req, res) => {
+  const { id } = req.params;
+  let data = students.map((student) => student);
+
+  data = data.filter((student) => student.id == id);
+  if (data.length == 0) {
+    return res.status(404).json({
+      message: `Student with id ${id} is not found!`,
+      data: null,
+    });
+  }
+
+  const response = {
+    data: data[0],
+    message: null,
+  };
+
+  res.status(200).json(response);
+});
+
+app.post("/students", (req, res) => {
+  // validate the request from user
+  const { name, address } = req.body;
+  if (!name || name == "") {
+    return res.status(400).json({
+      data: null,
+      message: "Name must be filled!",
+    });
+  }
+  if (!address) {
+    return res.status(400).json({
+      data: null,
+      message: "Address must be filled!",
+    });
+  }
+
+  const { city, province } = address;
+  if (!city || city == "") {
+    return res.status(400).json({
+      data: null,
+      message: "City must be filled!",
+    });
+  }
+  if (!province || province == "") {
+    return res.status(400).json({
+      data: null,
+      message: "Province must be filled!",
+    });
+  }
+
+  /* Process insert data */
+  // get the last id and then add 1
+  const lastStudent = students[students.length - 1];
+  req.body = {
+    id: lastStudent.id + 1,
+    ...req.body,
+  };
+
+  // Insert to data student
+  students.push(req.body);
+
+  res.status(201).json({
+    data: req.body,
+    message: null,
+  });
+});
+
+/* Mini Challenge: API to Update (PUT the Data) */
+app.put("/students/:id", (req, res) => {
+  // validate the request from user
+  const { name, address } = req.body;
+  if (!name || name == "") {
+    return res.status(400).json({
+      data: null,
+      message: "Name must be filled!",
+    });
+  }
+
+  const { city, province } = address;
+  if (!city || city == "") {
+    return res.status(400).json({
+      data: null,
+      message: "City must be filled!",
+    });
+  }
+  if (!province || province == "") {
+    return res.status(400).json({
+      data: null,
+      message: "Province must be filled!",
+    });
+  }
+
+  const id = parseInt(req?.params?.id);
+  const updatedStudent = {
+    id: id,
+    ...req.body,
+  };
+
+  // Update the data by id
+  students.map((student, index) => {
+    if (student?.id == id) {
+      students[index] = updatedStudent;
+    }
+  });
+
+  res.status(200).json({
+    data: updatedStudent,
+    message: null,
+  });
+});
+
+app.delete("/students/:id", (req, res) => {
+  const id = parseInt(req?.params?.id);
+
+  // Mini Challenge: Delete here, you can do with filter or for or another method
+  index = students.findIndex((student) => student.id === id);
+  students.splice(index, 1);
+
+  // Response
+  res.status(200).json({
+    data: null,
+    message: "Success",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}!`);
+});
---
+++ public/scripts/index.js
@@ -0,0 +1,31 @@
+import students from "./students.js";
+
+const studentCard = document.getElementById("student-card");
+const searchInput = document.getElementById("search-input");
+const searchSubmit = document.getElementById("search-submit");
+
+// Get students
+async function getStudents(name) {
+  const studentsData = await students.getStudentsData(name);
+  let studensDataInCards = "";
+  studentsData.map((student) => {
+    studensDataInCards += `<div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${student.name}</h5>
+                            <p class="card-text">${student.address.city}, ${student.address.province}</p>
+                        </div>
+                    </div>
+                </div>`;
+  });
+  studentCard.innerHTML = studensDataInCards;
+}
+
+searchSubmit.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = searchInput.value;
+  getStudents(name);
+});
+
+getStudents("");
